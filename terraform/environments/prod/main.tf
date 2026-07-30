f78b0fe8fbf2827c621826dc3052fd1718977cc2
Eran Meir <Eran Meir>
2026-07-31 00:44:43 +0300

    Fix: Remove unused private_key variable declaration to prevent Terraform crash

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -11,7 +11,7 @@ terraform {
 variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
-variable "private_key" {}
+
 variable "region" {}
 variable "compartment_ocid" {}
 variable "ssh_public_key" {}
