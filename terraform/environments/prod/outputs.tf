25dab7df11581bf21e0e3d0472417140a11b6be7
Eran Meir <Eran Meir>
2026-07-31 02:27:58 +0300

    Add Terraform outputs to print Server IP and Web Client URL

---
+++ terraform/environments/prod/outputs.tf
@@ -0,0 +1,9 @@
+output "server_public_ip" {
+  description = "The raw public IP address of the server"
+  value       = module.prod_cluster.instance_public_ips[0]
+}
+
+output "web_client_url" {
+  description = "Direct link to the future web client"
+  value       = "http://${module.prod_cluster.instance_public_ips[0]}"
+}
