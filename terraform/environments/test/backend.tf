8a1da37085a8b6278299fd1de35cb4733d06402c
Eran Meir <Eran Meir>
2026-07-31 00:09:27 +0300

    Fix Terraform OCI S3 backend invalid region error

--- terraform/environments/test/backend.tf
+++ terraform/environments/test/backend.tf
@@ -6,6 +6,7 @@ terraform {
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
     use_path_style              = true
     skip_s3_checksum            = true
     key                         = "test/terraform.tfstate"
